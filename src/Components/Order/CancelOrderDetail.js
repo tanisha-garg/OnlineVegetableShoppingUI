d15bf45e56339af461a7c1337b957fe570e60c22
tanisha-garg <tanishagarg804@gmail.com>
2021-04-22 11:42:17 +0530

    Changes done

--- src/Components/Order/CancelOrderDetail.js
+++ src/Components/Order/CancelOrderDetail.js
@@ -1,5 +1,6 @@
 import { useState } from "react";
 const CancelOrderDetail = () => {
+  
   const orderList = [
     { id: 1, name: "Sameer" },
     { id: 2, name: "Suriya" },
--- src/Components/Order/ViewOrderDetail.js
+++ src/Components/Order/ViewOrderDetail.js
@@ -1,5 +1,6 @@
 import { useState } from "react";
 const ViewOrderDetail = () => {
+  
   const orderList = [
     { id: 1, name: "Sameer" },
     { id: 2, name: "Suriya " },
