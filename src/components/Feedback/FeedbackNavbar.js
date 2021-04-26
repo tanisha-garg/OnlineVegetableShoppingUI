7dca37f1dcaf47fb1881c9ee4694b5bcdee11537
iamsfz <ss9823@srmist.edu.in>
2021-04-27 00:33:57 +0530

    pushing completed code

--- src/components/Feedback/FeedbackNavbar.js
+++ src/components/Feedback/FeedbackNavbar.js
@@ -3,13 +3,10 @@ import { Link } from "react-router-dom";
 function OrderNavbar() {
   return (
     <div className="container navbar mt-3">
-      <Link to="/feedbackhome/feedbackdetails/:id">Feedback Details by Feedbackid</Link>
+      <Link to="/feedbackhome/feedbackdetails/:id">Feedback Details by Feedback id</Link>
 
-      <Link to="/feedbackhome/feedbacklist/">
-        {" "}
-        List of Feedbacks {" "}
-      </Link>
-      
+      <Link to="/feedbackhome/feedbacklist/:id"> List of Feedbacks by Feedback id</Link>
+
       <Link to="/feedbackhome/feedbackbyvegetableid/:id">Feedback Details by Vegetable id</Link>
     </div>
   );
