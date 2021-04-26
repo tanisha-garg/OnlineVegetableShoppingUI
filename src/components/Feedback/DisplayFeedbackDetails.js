cc6716b0fd327bcbefa6ccbf29f4f771fc7b7edc
iamsfz <ss9823@srmist.edu.in>
2021-04-27 00:29:27 +0530

    pushing completed code

--- src/components/Feedback/DisplayFeedbackDetails.js
+++ src/components/Feedback/DisplayFeedbackDetails.js
@@ -5,6 +5,8 @@ export default function DisplayFeedbackDetails({ feed }) {
     return (
 
         <div>
+            Feedback Id is{feedbackId}
+            Customer Id is {customerId}
             Vegetable Id is {vegetableId}
             <br />
             Feedback is {feedback}
