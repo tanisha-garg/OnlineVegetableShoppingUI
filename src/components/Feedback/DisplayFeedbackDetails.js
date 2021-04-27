f991fcea6d97a1dbd1ea9826e0ac5c5cc4c654d0
iamsfz <ss9823@srmist.edu.in>
2021-04-27 12:27:50 +0530

    pushing code

--- src/components/Feedback/DisplayFeedbackDetails.js
+++ src/components/Feedback/DisplayFeedbackDetails.js
@@ -1,18 +1,20 @@
 export default function DisplayFeedbackDetails({ feed }) {
 
-    let { feedbackId,customerId,vegetableId,feedback,rating} = feed;
+    let { feedbackId,vegetableId,feedback,rating,comments} = feed;
 
     return (
 
         <div>
             Feedback Id is{feedbackId}
-            Customer Id is {customerId}
+            <br/>
             Vegetable Id is {vegetableId}
             <br />
             Feedback is {feedback}
             <br />
             Rating is {rating}
             <br />
+            Comments is{comments}
+            <br/>
         </div>
     );
 }
